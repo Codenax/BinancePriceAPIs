f111372d35eff1e7234bf002c28922a3bdb5fbf9
Jahangir Alam <jahangir.vsv@gmail.com>
2025-11-06 15:42:31 +0600

    Initial commit4

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// CORS allow
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-// Binance Futures Spot/Perpetual price endpoint
 app.get('/futures/:symbol', async (req, res) => {
   const symbol = req.params.symbol.toUpperCase();
   const url = `https://fapi.binance.com/fapi/v1/ticker/price?symbol=${symbol}`;
-
   try {
     const response = await axios.get(url);
     res.json({ symbol: response.data.symbol, price: response.data.price });
@@ -24,5 +21,5 @@ app.get('/futures/:symbol', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Binance Futures proxy server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
